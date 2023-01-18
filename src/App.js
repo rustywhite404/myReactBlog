f296b6869142dd7db9e3356d3b36696ea3c48960
ejpark <ejpark92@hunesion.com>
2023-01-18 17:28:24 +0900

    study react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='black-nav'>
+        <h4>블로그 헤더</h4>
+      </div>
+    </div>
+  );
+}
+
+export default App;
